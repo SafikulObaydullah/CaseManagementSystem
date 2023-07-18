433d63c2cd5eca5f71f11f51d2f4e3776684ab8e
obaydullah <obaydullah599@gmail.com>
2023-07-18 07:13:41 +0600

    Add project files.

---
+++ CaseManagementSystem/DbModel/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CaseManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseManagementSystem.DbModel
+{
+   public class ApplicationDbContext : DbContext
+   {
+      public ApplicationDbContext(DbContextOptions options) : base(options)
+      {
+      }
+      public DbSet<Lawyer> Lawyers { get; set; }   
+   }
+}
---
+++ CaseManagementSystem/Models/Lawyer.cs
@@ -0,0 +1,10 @@
+﻿namespace CaseManagementSystem.Models
+{
+   public class Lawyer
+   {
+      public int Id { get; set; }   
+      public string Name { get; set; }
+      public string Designation { get; set; }
+      public string Address { get; set; }
+   }
+}
