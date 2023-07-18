8f87b5e625f20848b54c6a01e92e79d236f4f6de
obaydullah <obaydullah599@gmail.com>
2023-07-18 07:15:43 +0600

    Controller added

---
+++ CaseManagementSystem/Controllers/LawyerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseManagementSystem.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class LawyerController : ControllerBase
+   {
+        public LawyerController()
+        {
+               
+        }
+      [HttpPost]
+      public string CreateLaywer()
+      {
+         return "";
+      }
+    }
+}
